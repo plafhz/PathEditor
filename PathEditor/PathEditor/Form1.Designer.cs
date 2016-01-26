947038c2fdd448c1b27821091a32ddebd00ff2fe
plafhz <plafhz@gmail.com>
2016-01-26 18:35:46 -0300

    Editar variable de entorno path del sistema
    Editar variable de entorno path del sistema, fácilmente y de manera
    grafica.

---
+++ PathEditor/PathEditor/Form1.Designer.cs
@@ -0,0 +1,121 @@
+﻿namespace PathEditor {
+    partial class Form1 {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing) {
+            if (disposing && (components != null)) {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent() {
+            this.lista = new System.Windows.Forms.ListBox();
+            this.txtValor = new System.Windows.Forms.TextBox();
+            this.btnGuardar = new System.Windows.Forms.Button();
+            this.btnEditar = new System.Windows.Forms.Button();
+            this.btnAgregar = new System.Windows.Forms.Button();
+            this.btnEliminar = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // lista
+            // 
+            this.lista.FormattingEnabled = true;
+            this.lista.Location = new System.Drawing.Point(13, 13);
+            this.lista.Name = "lista";
+            this.lista.Size = new System.Drawing.Size(429, 199);
+            this.lista.TabIndex = 0;
+            this.lista.SelectedIndexChanged += new System.EventHandler(this.lista_SelectedIndexChanged);
+            // 
+            // txtValor
+            // 
+            this.txtValor.Location = new System.Drawing.Point(13, 219);
+            this.txtValor.Name = "txtValor";
+            this.txtValor.Size = new System.Drawing.Size(429, 20);
+            this.txtValor.TabIndex = 1;
+            // 
+            // btnGuardar
+            // 
+            this.btnGuardar.Location = new System.Drawing.Point(367, 245);
+            this.btnGuardar.Name = "btnGuardar";
+            this.btnGuardar.Size = new System.Drawing.Size(75, 23);
+            this.btnGuardar.TabIndex = 2;
+            this.btnGuardar.Text = "Guadar";
+            this.btnGuardar.UseVisualStyleBackColor = true;
+            this.btnGuardar.Click += new System.EventHandler(this.btnGuardar_Click);
+            // 
+            // btnEditar
+            // 
+            this.btnEditar.Location = new System.Drawing.Point(286, 246);
+            this.btnEditar.Name = "btnEditar";
+            this.btnEditar.Size = new System.Drawing.Size(75, 23);
+            this.btnEditar.TabIndex = 3;
+            this.btnEditar.Text = "Editar";
+            this.btnEditar.UseVisualStyleBackColor = true;
+            this.btnEditar.Click += new System.EventHandler(this.btnEditar_Click);
+            // 
+            // btnAgregar
+            // 
+            this.btnAgregar.Location = new System.Drawing.Point(205, 244);
+            this.btnAgregar.Name = "btnAgregar";
+            this.btnAgregar.Size = new System.Drawing.Size(75, 23);
+            this.btnAgregar.TabIndex = 4;
+            this.btnAgregar.Text = "Agregar";
+            this.btnAgregar.UseVisualStyleBackColor = true;
+            this.btnAgregar.Click += new System.EventHandler(this.btnAgregar_Click);
+            // 
+            // btnEliminar
+            // 
+            this.btnEliminar.Location = new System.Drawing.Point(124, 244);
+            this.btnEliminar.Name = "btnEliminar";
+            this.btnEliminar.Size = new System.Drawing.Size(75, 23);
+            this.btnEliminar.TabIndex = 5;
+            this.btnEliminar.Text = "Eliminar";
+            this.btnEliminar.UseVisualStyleBackColor = true;
+            this.btnEliminar.Click += new System.EventHandler(this.btnEliminar_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(454, 296);
+            this.Controls.Add(this.btnEliminar);
+            this.Controls.Add(this.btnAgregar);
+            this.Controls.Add(this.btnEditar);
+            this.Controls.Add(this.btnGuardar);
+            this.Controls.Add(this.txtValor);
+            this.Controls.Add(this.lista);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Path Editor";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox lista;
+        private System.Windows.Forms.TextBox txtValor;
+        private System.Windows.Forms.Button btnGuardar;
+        private System.Windows.Forms.Button btnEditar;
+        private System.Windows.Forms.Button btnAgregar;
+        private System.Windows.Forms.Button btnEliminar;
+    }
+}
+
---
+++ PathEditor/PathEditor/Form1.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PathEditor {
+    public partial class Form1 : Form {
+        private const string TEXTO_CONFIRMACION_ELIMINAR = "¿Esta seguro que desea eliminar el elemento?";
+        private const string TEXTO_CONFIRMACION_EDITAR = "¿Esta seguro que desea modificar el elemento?";
+        private const string TEXTO_CONFIRMACION_GUARDAR = "¿Esta seguro que desea guardar los cambios en el sistema?";
+        private const string TEXTO_ERROR_LISTA = "Debe seleccionar un elemento de la lista.";
+        private const string TEXTO_ERROR_TEXTO = "Debe escribir la ruta en el cuadro de texto.";
+        private const string TEXTO_LISTA_VACIA = "Lista vacia.";
+        private const string CAPTION_ADVERTENCIA = "Advertencia";
+ 
+        public Form1() {
+            InitializeComponent();
+            llenarLista();
+        }
+
+        public void backup() {
+            string cadena = convertirCadena(getValues());
+            string fecha = DateTime.Today.Day.ToString() + "-" + DateTime.Today.Month.ToString() + "-" + DateTime.Today.Year.ToString();
+            string hora = DateTime.Now.Hour.ToString() + "-" + DateTime.Now.Minute.ToString() + "-" + DateTime.Now.Second.ToString();
+            string path = "BACKUP-"+ fecha + "-"+ hora +".txt";
+            System.IO.File.WriteAllText(@path, cadena);
+        }
+
+        public string convertirCadena(string[] valores) {
+            string completo = "";
+            foreach (string s in valores) {
+                if (valores.Last() != s) {
+                    completo += s + ";";
+                } else {
+                    completo += s;
+                }
+            }
+            return completo;
+        }
+
+        public string[] getValues() {
+            return Environment.GetEnvironmentVariable("Path", EnvironmentVariableTarget.Machine).Split(';');
+        }
+
+        public void setValues(string[] valores) {
+            string valor = convertirCadena(valores);
+            Environment.SetEnvironmentVariable("Path", valor, EnvironmentVariableTarget.Machine);
+        }
+
+        public string[] listaArray() {
+            string[] valores = new string[lista.Items.Count];
+            for (int i = 0; i < valores.Length; i++) {
+                valores[i] = lista.Items[i].ToString();
+            }
+            return valores;
+        }
+
+        public void llenarLista() {
+            lista.Items.Clear();
+            lista.Items.AddRange(getValues());
+        }
+
+        private void lista_SelectedIndexChanged(object sender, EventArgs e) {
+            if (lista.SelectedItem != null) {
+                txtValor.Text = lista.SelectedItem.ToString();
+            }
+        }
+
+        private void btnEditar_Click(object sender, EventArgs e) {
+            if (!txtValor.Text.Equals(String.Empty)) {
+                if (lista.SelectedItem != null) {
+                    DialogResult resultado = MessageBox.Show(TEXTO_CONFIRMACION_EDITAR, CAPTION_ADVERTENCIA, MessageBoxButtons.YesNo, MessageBoxIcon.Question);
+                    if (resultado == DialogResult.Yes) {
+                        int indexActual = lista.SelectedIndex;
+                        lista.Items.RemoveAt(lista.SelectedIndex);
+                        lista.Items.Insert(indexActual, txtValor.Text);
+                        txtValor.Text = String.Empty;
+                    }
+                } else {
+                    MessageBox.Show(TEXTO_ERROR_LISTA, CAPTION_ADVERTENCIA, MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+                }
+            } else {
+                MessageBox.Show(TEXTO_ERROR_TEXTO, CAPTION_ADVERTENCIA, MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+            }
+            
+        }
+
+        private void btnAgregar_Click(object sender, EventArgs e) {
+            if (!txtValor.Text.Equals(String.Empty)) {
+                lista.Items.Add(txtValor.Text);
+                txtValor.Text = String.Empty;
+            } else {
+                MessageBox.Show(TEXTO_ERROR_TEXTO, CAPTION_ADVERTENCIA, MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+            }
+        }
+
+        private void btnEliminar_Click(object sender, EventArgs e) {
+            if (lista.SelectedItem != null) {
+                DialogResult resultado = MessageBox.Show(TEXTO_CONFIRMACION_ELIMINAR, CAPTION_ADVERTENCIA, MessageBoxButtons.YesNo, MessageBoxIcon.Question);
+                if (resultado == DialogResult.Yes) {
+                    lista.Items.Remove(lista.SelectedItem);
+                    txtValor.Text = String.Empty;
+                }
+            }else {
+                MessageBox.Show(TEXTO_ERROR_LISTA, CAPTION_ADVERTENCIA, MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+            }
+        }
+
+        private void btnGuardar_Click(object sender, EventArgs e) {
+            if (lista.Items.Count > 0) {
+                DialogResult resultado = MessageBox.Show(TEXTO_CONFIRMACION_GUARDAR, CAPTION_ADVERTENCIA, MessageBoxButtons.YesNo, MessageBoxIcon.Question);
+                if (resultado == DialogResult.Yes) {
+                    backup();
+                    setValues(listaArray());
+                    llenarLista();
+                }
+            } else {
+                MessageBox.Show(TEXTO_LISTA_VACIA, CAPTION_ADVERTENCIA, MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+            }
+        }
+    }
+}
